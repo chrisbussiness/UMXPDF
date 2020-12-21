2e4c3c9e0a331552aeddcaf98d777ea212c4e7fc
IOSDeveloper <info@chris.my>
2020-12-21 17:55:11 +0800

    Commit

--- Pod/Classes/Renderer/PDFViewController.swift
+++ Pod/Classes/Renderer/PDFViewController.swift
@@ -255,6 +255,7 @@ open class PDFViewController: UIViewController {
     }
     
     func showActivitySheet() {
+        annotationController.finishAnnotation()
         let renderer = PDFRenderController(document: document, controllers: [
             annotationController,
             formController
